1a13add5fd3d3d01ec572a927ea84a5fb2a09394
chrislisbon <yulashka.htm@yandex.ru>
2025-03-11 12:39:21 +0300

    plot metrics

--- segmentation/plot_metrics.py
+++ segmentation/plot_metrics.py
@@ -1,21 +1,58 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 df = pd.read_csv('validation/metrics.csv')
 df = df.sort_values(['cells_num'])
+df = df[df['cells_num'] < 100]
 
-plt.scatter(df['cells_num'], df['MAE'])
-plt.xlabel('Cells number')
-plt.ylabel('MAE')
-plt.xlim(10, 55)
-plt.ylim(-1, 20)
-plt.title('Metric dependence of cells number')
+# MAE METRIC
+values = []
+labels = []
+bins = np.arange(20, 50, 5)
+for i in range(len(bins[:-1])):
+    ts = df[(bins[i] <= df['cells_num']) & (df['cells_num'] < bins[i+1])]['MAE'].values
+    labels.append(f'{bins[i]}-{bins[i+1]}')
+    values.append(ts)
+
+fig, axs = plt.subplots(1, 2, figsize=(10, 4))
+axs[0].scatter(df['cells_num'], df['MAE'])
+axs[0].set_ylabel('MAE')
+axs[0].set_xlabel('Cells number')
+axs[0].set_title('Each point is image')
+
+axs[1].boxplot(values, tick_labels=labels)
+axs[1].set_ylabel('MAE')
+axs[1].set_xlabel('Cells number')
+axs[1].set_title('Distribution by bins')
+
+fig.suptitle('Metric dependence of cells number')
+plt.tight_layout()
+plt.savefig('validation/MAE_distribution.png')
 plt.show()
 
-plt.scatter(df['cells_num'], df['IOU'])
-plt.xlabel('Cells number')
-plt.ylabel('IOU')
-plt.xlim(10, 55)
-plt.ylim(0.96, 1)
-plt.title('Metric dependence of cells number')
+
+# IOU METRIC
+values = []
+labels = []
+bins = np.arange(20, 50, 5)
+for i in range(len(bins[:-1])):
+    ts = df[(bins[i] <= df['cells_num']) & (df['cells_num'] < bins[i+1])]['IOU'].values
+    labels.append(f'{bins[i]}-{bins[i+1]}')
+    values.append(ts)
+
+fig, axs = plt.subplots(1, 2, figsize=(10, 4))
+axs[0].scatter(df['cells_num'], df['IOU'])
+axs[0].set_ylabel('IOU')
+axs[0].set_xlabel('Cells number')
+axs[0].set_title('Each point is image')
+
+axs[1].boxplot(values, tick_labels=labels)
+axs[1].set_ylabel('IOU')
+axs[1].set_xlabel('Cells number')
+axs[1].set_title('Distribution by bins')
+
+fig.suptitle('Metric dependence of cells number')
+plt.tight_layout()
+plt.savefig('validation/IoU_distribution.png')
 plt.show()
