e3246351e2d5087a9523b746129c7890e10b9266
chrislisbon <yulashka.htm@yandex.ru>
2025-03-21 16:24:40 +0300

    cfu

--- CellCounter/validation.py
+++ CellCounter/validation.py
@@ -1,9 +1,11 @@
 import os
 
+import pandas as pd
+
 from segmentation.cv_algorithms import detect_cv2_objs
 
+df = pd.read_csv('D:/microalgae_data/target_concentration.csv')
+df
+
 folder_path = 'D:/microalgae_data'
-for samp_name in os.listdir(folder_path):
-    if '.zip' not in samp_name:
-        for img in os.listdir(samp_name):
-            cells_objs = detect_cv2_objs(img)
+
